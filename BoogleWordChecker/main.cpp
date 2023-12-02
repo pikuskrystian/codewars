91cae9b297807eebe7e5939ddc3716079c31fe25
pikuskrystian <pikuskrystian@gmail.com>
2023-12-02 20:29:15 +0100

    new version of solution

--- BoogleWordChecker/main.cpp
+++ BoogleWordChecker/main.cpp
@@ -3,48 +3,54 @@
 
 using namespace std;
 
-bool isValidCell(int row, int col, int numRows, int numCols, vector<vector<bool>>& visited) {
-    return row >= 0 && row < numRows && col >= 0 && col < numCols && !visited[row][col];
+bool is_valid_move(int row, int col, const vector<vector<char>> &board, vector<vector<bool>> &visited) {
+    int rows = board.size();
+    int cols = board[0].size();
+
+    return (row >= 0 && row < rows && col >= 0 && col < cols && !visited[row][col]);
 }
 
-bool isValidGuess(const vector<vector<char>>& board, const string& word, int row, int col, int index, vector<vector<bool>>& visited) {
+bool search_word(int row, int col, const vector<vector<char>> &board, const string &word, int index, vector<vector<bool>> &visited) {
     if (index == word.length()) {
-        return true;
+        return true; // Successfully found the entire word
     }
 
-    if (!isValidCell(row, col, board.size(), board[0].size(), visited) || board[row][col] != word[index]) {
-        return false;
+    static const int directions[][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
+    for (int dir = 0; dir < 8; ++dir) {
+        int newRow = row + directions[dir][0];
+        int newCol = col + directions[dir][1];
+
+        if (is_valid_move(newRow, newCol, board, visited) && board[newRow][newCol] == word[index]) {
+            visited[newRow][newCol] = true;
+
+            if (search_word(newRow, newCol, board, word, index + 1, visited)) {
+                return true; // Word found in this direction
+            }
+
+            visited[newRow][newCol] = false; // Backtrack
+        }
     }
 
-    visited[row][col] = true;
+    return false; // Word not found
+}
 
-    // Explore adjacent cells
-    for (int i = -1; i <= 1; ++i) {
-        for (int j = -1; j <= 1; ++j) {
-            if (isValidGuess(board, word, row + i, col + j, index + 1, visited)) {
-                visited[row][col] = false;  // Backtrack: mark the current cell as unvisited
-                return true;
+bool check_word(const vector<vector<char>> &board, const string &word) {
+    int rows = board.size();
+    int cols = board[0].size();
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (board[i][j] == word[0]) {
+                vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+                visited[i][j] = true;
+
+                if (search_word(i, j, board, word, 1, visited)) {
+                    return true; // Word found starting from this position
+                }
             }
         }
     }
 
-    visited[row][col] = false;  // Backtrack: mark the current cell as unvisited
-    return false;
+    return false; // Word not found on the board
 }
-
-bool check_word(const vector<vector<char>>& board, const string& word) {
-    int numRows = board.size();
-    int numCols = board[0].size();
-
-    for (int i = 0; i < numRows; ++i) {
-        for (int j = 0; j < numCols; ++j) {
-            vector<vector<bool>> visited(numRows, vector<bool>(numCols, false));
-            if (isValidGuess(board, word, i, j, 0, visited)) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
-
